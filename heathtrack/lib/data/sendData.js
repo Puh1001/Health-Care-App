71c21f863fc226162db6b9e2729378700ae5864e
Puh <hoangvn2phu@gmail.com>
2024-03-08 16:34:48 +0700

    update sendData.json, heathData

--- heathtrack/lib/data/sendData.js
+++ heathtrack/lib/data/sendData.js
@@ -1,38 +1,44 @@
 const fs = require("fs");
-const interval = 1000; // 30 minutes in milliseconds
+const data = require("./data.json");
 
-function sendData() {
-  fs.readFile("data.json", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading data.json:", err);
-      return;
-    }
-
-    const jsonData = JSON.parse(data);
-    const objectToSend = jsonData.shift(); // Remove the first object from the array
-
-    // Add current timestamp to the object
-    objectToSend.timestamp = Date.now();
-
-    fetch("http://localhost:8080/api/heath-data", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(objectToSend),
+function sendObjectToServer(objectToSend) {
+  objectToSend.timestamp = Date.now();
+  fetch("http://localhost:8080/api/heath-data", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(objectToSend),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.error("Error sending data:", response.status);
+      } else {
+        console.log("Data sent successfully!");
+      }
     })
-      .then((response) => {
-        if (!response.ok) {
-          console.error("Error sending data:", response.status);
-        } else {
-          console.log("Data sent successfully!");
-        }
-      })
-      .catch((error) => {
-        console.error("Error sending data:", error);
-      });
-  });
+    .catch((error) => {
+      console.error("Error sending data:", error);
+    });
 }
 
-sendData(); // Send the first object immediately
-setInterval(sendData, interval); // Schedule sending subsequent objects
+function sendObjectsPeriodically() {
+  let index = 0;
+  const interval = 1000; // 30 phút
+
+  const sendNextObject = () => {
+    if (index < data.length) {
+      sendObjectToServer(data[index]);
+      console.log(data[index]);
+      index++;
+    } else {
+      clearInterval(timer);
+      console.log("Đã gửi hết các đối tượng trong mảng.");
+    }
+  };
+
+  sendNextObject(); // Gửi đối tượng đầu tiên ngay khi bắt đầu
+  const timer = setInterval(sendNextObject, interval);
+}
+
+sendObjectsPeriodically();
