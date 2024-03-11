f3a295c8cb4bedda8ceb971a6e7cf2e6a40c9a41
toai-nguyen <maxngudan@gmail.com>
2024-03-11 13:31:26 +0700

    add postNotification

--- heathtrack/lib/server/routers/postNotification.js
+++ heathtrack/lib/server/routers/postNotification.js
@@ -1,36 +1,36 @@
 const express = require("express");
-
+const notiRouter = express.Router();
 const {initializeApp, cert} = require('firebase-admin/app');
-
 require('dotenv');
+const admin = require('firebase-admin');
 
-const device_token_emulator = process.env.DEVICE_TOKEN;
-
-var serviceAccount = require('../../data/health-care-project-f7c4d-firebase-adminsdk-pt7fy-faad4d8dfc.json');
-
-var admin = require('firebase-admin');
-
-initializeApp({
-    credential: cert(serviceAccount)
+notiRouter.post("/api/notications", async(req, res)=> {
+    const device_token_emulator = process.env.DEVICE_TOKEN;
+    
+    try {
+        async function sendToDevice(token, data) {
+            await admin.messaging().sendToDevice(
+                token,
+                {
+                    data: data
+                },
+                {
+                    contentAvailable: true,
+                    priority: "high",
+                }
+            )
+        }
+        sendToDevice(device_token_emulator, message)
+    } catch (err) {
+        res.status(500).json({Error: err.message});
+    }
 })
 
-async function sendToDevice(token, data) {
-    await admin.messaging().sendToDevice(
-        token,
-        {
-            data: data
-        },
-        {
-            contentAvailable: true,
-            priority: "high",
-        }
-    )
-}
-
+// initializeApp({
+//     credential: cert(serviceAccount)
+    
+// })
 
 message = {
-
     message: "hello",
 }
-
-sendToDevice(device_token_emulator, message)
